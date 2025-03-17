a6a766ca9c33e2e4e7e9eb209c44c4ccd9a7e0ae
Greice <silveiragk@gmail.com>
2025-03-17 20:03:57 +1300

    Added a second print statement for feature branch update

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Kia ora! I'm Greice, and this is my BIT504 Task 9.");
         System.out.println("Super excited to demonstrate my Java and GitHub skills!");
         System.out.println("This update is made on my first branch!");
+        System.out.println("This is my second update on the feature branch!");
     }
 }
