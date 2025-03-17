b0711d002bd47dcfd042ff764f175e03b4b3867e
Greice <silveiragk@gmail.com>
2025-03-17 19:35:47 +1300

    Fixed syntax issue and updated welcome message

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Kia ora! I'm Greice, and this is my BIT504 Task 9.");
         System.out.println("Super excited to demonstrate my Java and GitHub skills!");
+        System.out.println("This update is made on my first branch!");
     }
 }
