2a5c8c5d8453e8ff0a31240b2e28b99fb5390268
Greice <silveiragk@gmail.com>
2025-03-17 19:14:47 +1300

    Initial commit with Main.java

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Kia ora! I'm Greice, and this is my BIT504 Task 9.");
+        System.out.println("Super excited to demonstrate my Java and GitHub skills!");
+    }
+}
